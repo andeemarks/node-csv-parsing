273b4a4fda8f25e4466de863420cd3a8fba1abb4
Andy Marks <vampwillow@gmail.com>
2017-12-28 13:51:39 +1100

    Refactor and styling

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 const http = require('http');
+const csv = require('csvtojson');
 
-const csv=require('csvtojson')
-
-const hostname = '127.0.0.1';
-const port = 3000;
+const HOSTNAME = '127.0.0.1';
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  	res.statusCode = 200;
-  	res.setHeader('Content-Type', 'text/plain');
-  	res.end('Hello World\n');
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World\n');
 });
 
-var numberOfIngestedRows = 0;
+const CSVFILEPATH = './csv/Fielding.csv'
 
-const csvFilePath='./csv/Fielding.csv'
-console.time("ingest");
-csv({noheader:true, headers: ['player-id', 'year-id', 'stint', 'team-id', 'lg-id', 'pos', 'g', 'gs', 'innouts', 'po', 'a', 'e', 'dp', 'pb', 'wp', 'sb', 'cs', 'pickoffs', 'zr', 'missing-g-c', 'missing-g']}).fromFile(csvFilePath).on('json',(json)=>{
-    numberOfIngestedRows = numberOfIngestedRows + 1
-    // console.log(json)
-}).on('done',(error)=>{
-    console.log(numberOfIngestedRows)
-	console.timeEnd("ingest");
-})
+function ingest(fileToIngest) {
+	console.log("Ingesting " + fileToIngest);
+	
+	var numberOfIngestedRows = 0;
 
-server.listen(port, hostname, () => {
-  	console.log(`Server running at http://${hostname}:${port}/`);
+	const COLUMN_NAMES = ['player-id', 'year-id', 'stint', 'team-id', 'lg-id', 'pos', 'g', 'gs', 'innouts', 'po', 'a', 'e', 'dp', 'pb', 'wp', 'sb', 'cs', 'pickoffs', 'zr', 'missing-g-c', 'missing-g'];
+
+	console.time('ingest');
+
+	csv({noheader:true, headers: COLUMN_NAMES})
+		.fromFile(CSVFILEPATH)
+		.on('json',(json) => {
+	  	numberOfIngestedRows = numberOfIngestedRows + 1
+	    // console.log(json)
+		})
+		.on('done', (error) => {
+		  console.log(numberOfIngestedRows)
+
+			console.timeEnd('ingest');
+		});
+}
+
+server.listen(PORT, HOSTNAME, () => {
+	ingest(CSVFILEPATH);
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
